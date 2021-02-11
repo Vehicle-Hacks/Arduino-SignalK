5e2acc34f2c5b5348f2d38aa6cbf73f26cd969d7
Ralph Grewe <rgdev@online.de>
2021-02-11 20:24:39 +0100

    Added new header and made WiFi credentials consistent to documentation.

--- Arduino-ESP8266-SignalK/Arduino-ESP8266-SignalK.ino
+++ Arduino-ESP8266-SignalK/Arduino-ESP8266-SignalK.ino
@@ -1,12 +1,19 @@
-/* 20191103 Ralph Grewe */
-/* This is a Arduino sketch which reads in analog values and sends out SignalK packages by UDP */
-/* Its based on: The Arduino UDPSendReceiveString example from Nov 3rd 2019: */ 
-/* https://www.arduino.cc/en/Tutorial/UDPSendReceiveString */
-/* The Arduino UdpNtpClient example from Nov. 25th 2019: */
-/* https://www.arduino.cc/en/Tutorial/UdpNtpClient */
-/* And a stackoverflow post about converting unix timestamp to a date: */
-/* https://stackoverflow.com/questions/7136385/calculate-day-number-from-an-unix-timestamp-in-a-math-way */
+/*   Arduino SignalK
+ *   Copyright (C) 2019 Ralph Grewe
+ *  
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 // For WiFi communication
 #include <ESP8266WiFi.h>
@@ -30,13 +37,13 @@ WiFiClient client;
 
 /* For SignalK data*/
 const unsigned int signalkPort = 8375;      // SignalK Port which is the same for TCP and UDP
-IPAddress signalkServer(192, 168, 177, 1);
+IPAddress signalkServer(192, 168, 4, 1);
 
 const char subscriptionString[] = "{\"context\":\"vessels.self\",\"subscribe\":[{\"path\":\"navigation.speedThroughWater\"},{\"path\":\"environment.depth.belowTransducer\"}]}";
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(57600);
+  Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -50,7 +57,7 @@ void setup() {
 
   //-------------------- Setup WiFi ---------------------------
 
-  WiFi.begin("MarineNetwork", "Marine!Passwort");
+  WiFi.begin("SignalkWlan", "SignalkWlan!Pwd");
   Serial.print("Connecting Wifi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
