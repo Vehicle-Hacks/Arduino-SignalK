8a91eb64219584ac9e199119da55e88a8b5b11c8
Ralph Grewe <rgdev@online.de>
2021-08-25 20:22:25 +0200

    Ethernet Shield and PROFET Shield are working together but when sending status to the SignalK server the Arduino gets stuck.

--- Arduino-AVR-Switching/Arduino-AVR-Switching.ino
+++ Arduino-AVR-Switching/Arduino-AVR-Switching.ino
@@ -20,39 +20,64 @@
 #include <EthernetClient.h>
 #include <ArduinoHttpClient.h>
 #include <ArduinoJson.h>
+#include <hss-board-arduino.hpp>
 
 /* Basic Ethernet variables*/
 byte mac[] = {0x90, 0xA2, 0xDA, 0x11, 0x05, 0xAA};
-IPAddress signalkServer(192, 168, 178, 53); //SignalK Server Address
+IPAddress signalkServer(192, 168, 178, 33); //SignalK Server Address
 EthernetClient ethernetClient;
 WebSocketClient webSocketClient = WebSocketClient(ethernetClient, signalkServer, 3000);
 
 /* Switch Input */
-const int switchPin = 4;
+const int switchPin = 49;
 bool switchOutput = false;
 bool switchPressed = false;
 
 /* LED Output */
-const int ledPin = 5;
+const int ledPin = 48;
 int targetState = 0;
 bool ledBlinking = false;
 bool ledState = 0;
 unsigned long lastBlink = 0;
 
+/* Power Output by SmartFET'S */
+HssBoardIno HSS = HssBoardIno(&BTS7002);
+
 /* ------------------- SignalK Messages sent by Arduino ------------------- 
  *  We store the messages in flash and load them on usage to save some SRAM
 */
-const int maxStringLength = 128;
+const int maxStringLength = 512;
 char stringBuffer[maxStringLength];
 
-/* Signalk request header */
+/* Signalk request messages */
 const char string_0[] PROGMEM = "{"
                            "\"requestId\": \"";
 const char string_1[] PROGMEM = "\",\"put\":{"
                                   "\"path\":\"electrical.inputs.testinput\","
                                   "\"value\":";
 
-const char *const string_table[] PROGMEM = {string_0, string_1};
+/* SignalK Switch State messages */
+const char string_2[] PROGMEM = "{"
+                           "\"updates\": [{"
+                           "\"source\": {"
+                           "\"label\": \"Arduino SignalK\","
+                           "\"type\": \"signalk\""
+                           "},"
+                           "\"timestamp\": \"";
+/* Signalk values begin */
+const char string_3[] PROGMEM = "\",\"values\": [";
+/* Signalk message end */
+const char string_4[] PROGMEM = "] }] }"; //values "]", updates "}]", final "}"
+
+/* SignalK tank level message */
+const char string_5[] PROGMEM = "{"
+                              "\"path\": \"electrical.load.1.\",";
+const char string_6[] PROGMEM = "voltage";
+const char string_7[] PROGMEM = "current";
+const char string_8[] PROGMEM = "\"value\":";                                 
+
+const char *const string_table[] PROGMEM = {string_0, string_1, string_2, string_3, string_4, string_5, string_6, string_7, string_8};
+
 
 /* ----------------------------- Actual Code  -------------------------------- */
 void switchStateChanged(int state) {
@@ -104,8 +129,12 @@ void setup() {
   //start Websocket client
   webSocketClient.begin(F("/signalk/v1/stream?subscribe=none"));
 
+  //initialize switch and LED
   pinMode(switchPin, INPUT);
   pinMode(ledPin, OUTPUT);
+
+  //initialize SmartFET Board
+  HSS.init();
 
   lastBlink = millis();
 }
@@ -169,4 +198,55 @@ void loop() {
       lastBlink = millis();
     }
   }
+
+  // Send switch data
+  webSocketClient.beginMessage(TYPE_TEXT);
+  strcpy_P(stringBuffer, (char *)pgm_read_word(&(string_table[2])));
+//  webSocketClient.print(stringBuffer);
+//  Serial.print(stringBuffer);
+  webSocketClient.print("0");
+//  Serial.print("0");
+  strcpy_P(stringBuffer, (char *)pgm_read_word(&(string_table[3])));
+  webSocketClient.print(stringBuffer);
+//  Serial.print(stringBuffer);
+  /* Voltage */
+  strcpy_P(stringBuffer, (char *)pgm_read_word(&(string_table[5])));
+  webSocketClient.print(stringBuffer);
+//  Serial.print(stringBuffer);
+  strcpy_P(stringBuffer, (char *)pgm_read_word(&(string_table[6])));
+  webSocketClient.print(stringBuffer);
+//  Serial.print(stringBuffer);
+
+  float value = HSS.readVss();
+  webSocketClient.print(String(value));
+//  Serial.print(value);
+  webSocketClient.print("},");
+//  Serial.print("},");
+
+  /* Current */
+  strcpy_P(stringBuffer, (char *)pgm_read_word(&(string_table[5])));
+  webSocketClient.print(stringBuffer);
+//  Serial.print(stringBuffer);
+  strcpy_P(stringBuffer, (char *)pgm_read_word(&(string_table[7])));
+  webSocketClient.print(stringBuffer);
+//  Serial.print(stringBuffer);
+  
+  int counter = 1;
+  value = HSS.readIsx(counter);
+  webSocketClient.print(String(value));
+//  Serial.print(value);
+  
+  /* End of message */
+  strcpy_P(stringBuffer, (char *)pgm_read_word(&(string_table[4])));
+  webSocketClient.print(stringBuffer);
+//  Serial.print(stringBuffer);
+  
+
+  int result = webSocketClient.endMessage();
+  if (result) {
+    Serial.print(F("Error writing packet: "));
+    Serial.println(result);
+  }  
+  
+  
 }
